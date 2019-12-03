7e0bb7c8b4bc53aa13e37c320e066cba66f7f599
Pranav Chaudhari <awesomesauceproduction@gmail.com>
2019-12-03 15:13:50 -0800

    init

---
+++ src/components/Landing/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Landing = () => (
+  <div>
+    <h1>Langin</h1>
+  </div>
+);
+export default Landing;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Nav = () => (
+  <div>
+    <h1>Nav</h1>
+  </div>
+);
+export default Nav;
